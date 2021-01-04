981b057ce02ab1e4b0bd7c12f8f518a3ad9aa2df
Admin <rkdxowhd98@naver.com>
2021-01-04 21:22:11 +0900

    MapView, MapFragment 완성

--- HongKaThon/app/src/main/java/com/cookandroid/social_distance/fragment/MapFragment.kt
+++ HongKaThon/app/src/main/java/com/cookandroid/social_distance/fragment/MapFragment.kt
@@ -1,8 +1,42 @@
 package com.cookandroid.social_distance.fragment
 
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import com.cookandroid.social_distance.R
 import com.cookandroid.social_distance.base.BaseFragment
 import com.cookandroid.social_distance.databinding.FragmentMapBinding
 
 class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_map_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.infection -> {
+                binding.graph.setSocialInfection()
+                true
+            }
+            R.id.level -> {
+                binding.graph.setSocialDistanceLevel()
+                true
+            }
+            R.id.filter -> {
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+    }
 }
---
+++ HongKaThon/app/src/main/java/com/cookandroid/social_distance/view/MapView.kt
@@ -0,0 +1,66 @@
+package com.cookandroid.social_distance.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import org.jsoup.Jsoup
+
+class MapView : WebView {
+    private var type = Type.NONE
+
+    private val socialDistanceLevel by lazy {
+        val document = runBlocking(Dispatchers.IO) {
+            Jsoup.connect("http://ncov.mohw.go.kr/regSocdisBoardView.do?brdId=6&brdGubun=68&ncvContSeq=495").get()
+        }
+
+        val graph = document.body().getElementById("reg_Map").toString()
+        val scripts = document.body().getElementsByTag("script").toString()
+
+        "<!DOCTYPE html><html lang=\"ko\">${document.head()}$graph$scripts</html>".replace("/static", "http://ncov.mohw.go.kr/static")
+    }
+
+    private val socialInfection by lazy {
+        val document = runBlocking(Dispatchers.IO) {
+            Jsoup.connect("http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=13&ncvContSeq=&contSeq=&board_id=&gubun=").get()
+        }
+        val graph = document.body().getElementsByClass("regional_patient_status_A")
+        val scripts = document.body().getElementsByTag("script")
+
+        "<!DOCTYPE html><html lang=\"ko\">${document.head()}$graph$scripts</html>".replace("/static", "http://ncov.mohw.go.kr/static")
+    }
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    init {
+        settings.javaScriptEnabled = true
+        webChromeClient = WebChromeClient()
+        webViewClient = WebViewClient()
+        settings.domStorageEnabled = true
+
+        setSocialInfection()
+    }
+
+    fun setSocialDistanceLevel() {
+        if (type != Type.LEVEL) {
+            type = Type.LEVEL
+            loadDataWithBaseURL(null, socialDistanceLevel, "text/html; charset=utf-8", "UTF-8", null)
+        }
+    }
+
+    fun setSocialInfection() {
+        if (type != Type.INFECTION) {
+            type = Type.INFECTION
+            loadDataWithBaseURL(null, socialInfection, "text/html; charset=utf-8", "UTF-8", null)
+        }
+    }
+
+    private enum class Type {
+        NONE, INFECTION, LEVEL
+    }
+}
