89916765253cdc4bb3ba5881e6beeedd8ae3aeab
Admin <rkdxowhd98@naver.com>
2021-01-04 20:32:57 +0900

    MainActivity Navigation 구조 완성

---
+++ HongKaThon/app/src/main/java/com/cookandroid/social_distance/fragment/MapFragment.kt
@@ -0,0 +1,8 @@
+package com.cookandroid.social_distance.fragment
+
+import com.cookandroid.social_distance.R
+import com.cookandroid.social_distance.base.BaseFragment
+import com.cookandroid.social_distance.databinding.FragmentMapBinding
+
+class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map) {
+}
