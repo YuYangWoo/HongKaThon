9d91ccc55dfdbfdf31e194cde4acb3e3e9f40e96
yuyw0712 <yuyw0712@naver.com>
2021-01-18 23:18:37 +0900

    안드로이드 11에 대응한 위치 권한 코드 리팩토링

--- HongKaThon/app/src/main/java/com/cookandroid/social_distance/SplashActivity.kt
+++ HongKaThon/app/src/main/java/com/cookandroid/social_distance/SplashActivity.kt
@@ -1,23 +1,22 @@
 package com.cookandroid.social_distance
 
-import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.LocationManager
+import android.net.Uri
+import android.os.Build
 import android.os.Handler
 import android.os.Looper
 import android.provider.Settings
 import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.cookandroid.social_distance.base.BaseActivity
 import com.cookandroid.social_distance.databinding.ActivitySplashBinding
 
 
 class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
     private val splashTime: Long = 2000
+
     var REQUIRED_PERMISSIONS = arrayOf(
             android.Manifest.permission.ACCESS_FINE_LOCATION,
             android.Manifest.permission.ACCESS_COARSE_LOCATION,
@@ -33,92 +32,82 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_spl
     private fun check() {
         if (!checkLocationServicesStatus()) {
             showDialogForLocationServiceSetting()
+//            // 비동기 처리 필요함.
+//            if(checkLocationServicesStatus()) {
+//                requestRuntimePermissions()
+//            }
         } else {
-            checkRunTimePermission()
+            requestRuntimePermissions()
         }
     }
 
-    // ActivityCompat.requestPermissions를 사용한 퍼미션 요청의 결과를 리턴
-    override fun onRequestPermissionsResult(
-            permsRequestCode: Int,
-            permissions: Array<String>,
-            grandResults: IntArray
-    ) {
-        if (permsRequestCode == PERMISSIONS_REQUEST_CODE && grandResults.size == REQUIRED_PERMISSIONS.size) {
-
-            // 요청 코드가 PERMISSIONS_REQUEST_CODE 이고, 요청한 퍼미션 개수만큼 수신되었다면
-            var check_result = true
-
-            // 모든 권한을 허용했는지 체크
-            for (result in grandResults) {
-                if (result != PackageManager.PERMISSION_GRANTED) {
-                    check_result = false
-                    break
-                }
-            }
-            if (check_result) {
-                // 2초 지나고 화면 전환
-                Handler(Looper.getMainLooper()).postDelayed({
-                    startActivity(Intent(this, MainActivity::class.java))
-                    finish()
-                }, splashTime)
-            } else {
-                // 거부한 권한이 있다면 앱을 종료
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])
-                        || ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[1])
-                        || ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[2])) {
-                    Toast.makeText(this, "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.", Toast.LENGTH_LONG).show()
-                    MainActivity().finish()
-                } else {
-                    Toast.makeText(this, "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ", Toast.LENGTH_LONG).show()
-                }
-            }
+    private fun requestRuntimePermissions() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return
         }
-    }
 
+        if (checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                &&checkSelfPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                checkSelfPermission(android.Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED) {
 
-    private fun checkRunTimePermission() {
-        // 위치 권한을 가지는지 Check
-        val hasFineLocationPermission = ContextCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-        )
-        val hasCoarseLocationPermission = ContextCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
-        )
-        val hasBackgroundLocationPermission = ContextCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
-        )
-        if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
-                hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED &&
-                hasBackgroundLocationPermission == PackageManager.PERMISSION_GRANTED) {
             // 2초 지나고 화면 전환
             Handler(Looper.getMainLooper()).postDelayed({
                 startActivity(Intent(this, MainActivity::class.java))
                 finish()
             }, splashTime)
+        } else {
+            requestPermissions(REQUIRED_PERMISSIONS,
+                    PERMISSION_REQUEST_CODE)
+        }
+    }
 
-        } else {  // 권한 허용을 한 적이 없으면
-            // 퍼미션 거부를 한 경우가 있다면
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])) {
-                // 권한 요청 이유 토스트 메시지
-                Toast.makeText(this, "이 앱을 실행하려면 위치 접근 권한이 필요합니다.", Toast.LENGTH_LONG)
-                        .show()
-                // 사용자에게 권한 요청
-                ActivityCompat.requestPermissions(
-                        this, REQUIRED_PERMISSIONS,
-                        PERMISSIONS_REQUEST_CODE
-                )
-            } else {
-                // 사용자가 퍼미션 거부를 한 적이 없는 경우에 권한 요청
-                ActivityCompat.requestPermissions(
-                        this, REQUIRED_PERMISSIONS,
-                        PERMISSIONS_REQUEST_CODE
-                )
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>,
+                                            grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isEmpty()) {
+                    throw RuntimeException("권한이 비어있습니다.")
+                }
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                        grantResults[1] == PackageManager.PERMISSION_GRANTED &&
+                        grantResults[2] == PackageManager.PERMISSION_GRANTED) {
+
+                    // 2초 지나고 화면 전환
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    }, splashTime)
+                } else {
+                    if (shouldShowRequestPermissionRationale(android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+                        Log.d(TAG, "사용자가 권한요청을 거부했습니다. 계속 요청하겠습니다.")
+                        requestPermissions(REQUIRED_PERMISSIONS,
+                                PERMISSION_REQUEST_CODE)
+                    } else {
+                        Log.d(TAG, "사용자가 거절했습니다.")
+                        showDialogToGetPermission()
+                    }
+                }
             }
         }
+    }
+
+    private fun showDialogToGetPermission() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("권한 요청")
+                .setMessage("앱을 사용하기 위해서는 권한 요청이 필요합니다. " +
+                        "OK를 누르면 권한 설정 창으로 이동합니다.")
+        builder.setPositiveButton("OK") { dialogInterface, i ->
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    Uri.fromParts("package", packageName, null))
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+        builder.setNegativeButton("Later") { dialogInterface, i ->
+            // ignore
+        }
+        val dialog = builder.create()
+        dialog.show()
     }
 
     // GPS 설정으로 이동
@@ -128,44 +117,30 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_spl
         builder.setMessage(
                 """
                 앱을 사용하기 위해서는 위치 서비스가 필요합니다.
-                위치 설정을 수정하실래요?
+                위치 서비스를 설정하시려면 설정을 눌러주세요
                 """.trimIndent()
         )
         builder.setCancelable(true)
-        builder.setPositiveButton("설정", DialogInterface.OnClickListener { dialog, id ->
+        builder.setPositiveButton("설정") { dialog, id ->
             val callGPSSettingIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
             startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE)
-        })
-        builder.setNegativeButton("취소",
-                DialogInterface.OnClickListener { dialog, id -> dialog.cancel() })
+        }
+        builder.setNegativeButton("취소"
+        ) { dialog, id -> dialog.cancel() }
         builder.create().show()
+
     }
 
-    // 결과 메서드
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        when (requestCode) {
-            GPS_ENABLE_REQUEST_CODE ->
-                //사용자가 GPS 활성 시켰는지 검사
-                if (checkLocationServicesStatus()) {
-                    if (checkLocationServicesStatus()) {
-                        Log.d("@@@", "onActivityResult : GPS 활성화 되있음")
-                        checkRunTimePermission()
-                        return
-                    }
-                }
-        }
-    }
 
     private fun checkLocationServicesStatus(): Boolean {
         val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
         return (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
                 || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
     }
-
     companion object {
         private const val GPS_ENABLE_REQUEST_CODE = 2001
-        private const val PERMISSIONS_REQUEST_CODE = 100
+        const val TAG = "SplashActivity"
+        const val PERMISSION_REQUEST_CODE = 1001
     }
 
 }
