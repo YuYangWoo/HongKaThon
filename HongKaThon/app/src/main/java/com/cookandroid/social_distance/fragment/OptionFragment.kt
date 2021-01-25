a488fe46cace9865e71a9117fbb15ccddafc4715
yuyw0712 <yuyw0712@naver.com>
2021-01-25 16:16:05 +0900

    Dialog 데이터 추가 + 스크롤뷰 추가

--- HongKaThon/app/src/main/java/com/cookandroid/social_distance/fragment/OptionFragment.kt
+++ HongKaThon/app/src/main/java/com/cookandroid/social_distance/fragment/OptionFragment.kt
@@ -29,8 +29,46 @@ class OptionFragment : BaseFragment<FragmentOptionBinding>(R.layout.fragment_opt
         optionList = arrayListOf(
             OptionItem("앱 버전","앱버전1.0"),
         OptionItem("위치정보 이용약관","위치제공한다."),
-        OptionItem("오픈소스 라이선스","어쩌구저쩌구"),
-        OptionItem("개발자 소개", "양유와 강태종")
+        OptionItem("오픈소스 라이선스","    implementation \"org.jetbrains.kotlin:kotlin-stdlib:$(kotlin_version)\"\n" +
+                "    implementation 'androidx.core:core-ktx:1.3.2'\n" +
+                "    implementation 'androidx.appcompat:appcompat:1.2.0'\n" +
+                "    implementation 'com.google.android.material:material:1.2.1'\n" +
+                "    implementation 'androidx.constraintlayout:constraintlayout:2.0.4'\n" +
+                "    implementation 'androidx.legacy:legacy-support-v4:1.0.0'\n" +
+                "    implementation 'com.google.firebase:firebase-database:19.6.0'\n" +
+                "    testImplementation 'junit:junit:4.+'\n" +
+                "    androidTestImplementation 'androidx.test.ext:junit:1.1.2'\n" +
+                "    androidTestImplementation 'androidx.test.espresso:espresso-core:3.3.0'\n" +
+                "\n" +
+                "    // Glide\n" +
+                "    implementation 'com.github.bumptech.glide:glide:4.11.0'\n" +
+                "    annotationProcessor 'com.github.bumptech.glide:compiler:4.11.0'\n" +
+                "\n" +
+                "    // Coroutine\n" +
+                "    implementation 'org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9'\n" +
+                "\n" +
+                "    // Volley\n" +
+                "    implementation 'com.android.volley:volley:1.1.1'\n" +
+                "\n" +
+                "    def lifecycle_version = \"2.2.0\"\n" +
+                "    // ViewModel\n" +
+                "    implementation \"androidx.lifecycle:lifecycle-viewmodel-ktx:$(lifecycle_version)\"\n" +
+                "\n" +
+                "    // LiveData\n" +
+                "    implementation \"androidx.lifecycle:lifecycle-livedata-ktx:$(lifecycle_version)\"\n" +
+                "\n" +
+                "    // RecyclerView Selection\n" +
+                "    implementation \"androidx.recyclerview:recyclerview-selection:1.1.0-rc03\"\n" +
+                "\n" +
+                "    implementation 'androidx.navigation:navigation-fragment-ktx:2.3.2'\n" +
+                "    implementation 'androidx.navigation:navigation-ui-ktx:2.3.2'\n" +
+                "\n" +
+                "    implementation 'org.jsoup:jsoup:1.13.1'\n" +
+                "\n" +
+                "    // Firebase\n" +
+                "    implementation platform('com.google.firebase:firebase-bom:26.2.0')\n" +
+                "    implementation 'com.google.firebase:firebase-analytics-ktx'"),
+        OptionItem("개발자 소개", "개발자 : YuYangWoo, KangTaeJong")
         )
     }
  }
